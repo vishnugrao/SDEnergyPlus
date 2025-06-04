59f8218647e4be237e28520ba12eb984fbfb9b8f
Vishnu Rao <79438943+vishnugrao@users.noreply.github.com>
2025-06-04 15:04:18 +0530

    db connection sorted

---
+++ server/db/connection.js
@@ -0,0 +1,25 @@
+import { ExplainVerbosity, MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+try {
+    await client.connect();
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+        "Pinged your deployment. You succesfully connected to MongoDB!"
+    );
+}
+catch(err) {
+    console.error(err);
+}
+
+let db = client.db('employees');
+
+export default db;
