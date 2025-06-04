ab9fbce8bcfd0dad1c9b93c39d1b5abeeb210906
Vishnu Rao <79438943+vishnugrao@users.noreply.github.com>
2025-06-04 16:36:46 +0530

    record route set in the router, need to fix deprecated ObjectId

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import records from "./routes/record.js";
+import records from "./routes/record.ts";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
